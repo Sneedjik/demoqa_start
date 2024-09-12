a333946d2b0aa9a92aa52594164012359e64e5ae
Sergei Volkov <Sneedjik@gmail.com>
2024-09-13 00:37:55 +0600

    added TextBoxTests

---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,36 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+//        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/text-box");
+        $("#userName").setValue("Bony");
+        $("#userEmail").setValue("Bony@man.com");
+        $("#currentAddress").setValue("Some place 1");
+        $("#permanentAddress").setValue("Another place 2");
+        $("#submit").click();
+
+        $("#output #name").shouldHave(text("Bony"));
+        $("#output #email").shouldHave(text("Bony@man.com"));
+        $("#output #currentAddress").shouldHave(text("Some place 1"));
+        $("#output #permanentAddress").shouldHave(text("Another place 2"));
+    }
+
+}
